72b654d1ae19eb0186078c6361c902725339d0ca
Kacper <109245559+kacpi-k@users.noreply.github.com>
2024-08-21 14:30:13 +0200

    final touches?

---
+++ src/components/Layout/Drawer/CustomScrollbar.tsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+const CustomDrawerScrollbar = styled.div`
+  overflow-y: auto;
+  max-height: 100%;
+
+  &::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: #e8edf7; 
+    border-radius: 10px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: #2196f3;
+    border-radius: 10px;
+    border: 2px solid #e8edf7;
+  }
+
+  &::-webkit-scrollbar-thumb:hover {
+    background-color: #1e88e5;
+  }
+`;
+
+export default CustomDrawerScrollbar;
